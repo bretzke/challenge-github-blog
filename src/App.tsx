5d39902d015974d30c1d73ba57e1efa7d31d5f95
Willian Bretzke <willianbretzke2013@hotmail.com>
2023-05-10 20:50:49 -0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter } from "react-router-dom";
+// import { ThemeProvider } from "styled-components";
+
+export default function App() {
+  return (
+    // <ThemeProvider theme={}>
+    <BrowserRouter>
+      {/* <CoffeesContextProvider>
+          <Router />
+        </CoffeesContextProvider> */}
+    </BrowserRouter>
+    // {/* <GlobalStyle /> */}
+    // </ThemeProvider>
+  );
+}
